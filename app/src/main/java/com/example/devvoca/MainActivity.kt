ceb8ffac6ea7a25b2177d287dccda74295b1d7db
Flasas <flasas@naver.com>
2023-03-14 23:05:19 +0900

    MainActivity.kt - View Binding 추가
    build.gradle
    Bottom navigation, View Binding 라이브러리 및 관련 코드 추가

--- app/src/main/java/com/example/devvoca/MainActivity.kt
+++ app/src/main/java/com/example/devvoca/MainActivity.kt
@@ -8,15 +8,22 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.devvoca.Repo.AppDatabase
 import com.example.devvoca.Repo.WordList
+import com.example.devvoca.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         Log.e("test","dd")
         val db = Room.databaseBuilder(this,AppDatabase::class.java,"word").build()
         val dao = db.wordDao()
@@ -26,8 +33,6 @@ class MainActivity : AppCompatActivity() {
                 dao.insert(WordList(0,"단어","번역","예제","타입"))
             }
         }
-
-        //nct 127 Ay-Yo
 
         findViewById<Button>(R.id.searchButton).setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
