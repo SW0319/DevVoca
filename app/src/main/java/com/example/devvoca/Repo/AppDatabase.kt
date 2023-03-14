6c58ae93e35d25fe2035571a0e5039c196ccbba4
anjihun <solutionwalker0319@gmail.com>
2023-03-14 21:44:13 +0900

    학습완료 단어 Entity 및 Dao 생성

--- app/src/main/java/com/example/devvoca/Repo/AppDatabase.kt
+++ app/src/main/java/com/example/devvoca/Repo/AppDatabase.kt
@@ -2,9 +2,11 @@ package com.example.devvoca.Repo
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.devvoca.Model.CompleteVocaDao
 import com.example.devvoca.Model.WordDao
 
-@Database(entities = [VocaList::class], version = 1)
+@Database(entities = [VocaList::class, CompleteVoca::class], version = 1)
 abstract class AppDatabase: RoomDatabase(){
     abstract fun wordDao(): WordDao
+    abstract fun completeVocaDao(): CompleteVocaDao
 }
