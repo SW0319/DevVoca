8b549be377abb2cd5901aad41b94f817983845a2
anjihun <solutionwalker0319@gmail.com>
2023-03-14 21:47:48 +0900

    선호단어 Entity 및 Dao 생성

--- app/src/main/java/com/example/devvoca/Repo/AppDatabase.kt
+++ app/src/main/java/com/example/devvoca/Repo/AppDatabase.kt
@@ -3,10 +3,12 @@ package com.example.devvoca.Repo
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.devvoca.Model.CompleteVocaDao
+import com.example.devvoca.Model.FavoriteVocaDao
 import com.example.devvoca.Model.WordDao
 
-@Database(entities = [VocaList::class, CompleteVoca::class], version = 1)
+@Database(entities = [VocaList::class, CompleteVoca::class, FavoriteVoca::class], version = 1)
 abstract class AppDatabase: RoomDatabase(){
     abstract fun wordDao(): WordDao
     abstract fun completeVocaDao(): CompleteVocaDao
+    abstract fun favoriteVocaDao() : FavoriteVocaDao
 }
