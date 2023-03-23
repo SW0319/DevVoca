c97bed17fb367dd4eefc68ae6a0277995696ca2b
Flasas <flasas@naver.com>
2023-03-23 22:04:12 +0900

    Main Activity Bottom Navi 추가 - 관련 요소 폴더 및 항목, xml 추가 완료
    기타 Fragment 화면 추가 진행 완료

--- app/src/main/java/com/example/devvoca/MainActivity.kt
+++ app/src/main/java/com/example/devvoca/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.devvoca.Model.DataModel
@@ -12,6 +16,7 @@ import com.example.devvoca.Repo.AppDatabase
 import com.example.devvoca.Repo.VocaList
 import com.example.devvoca.ViewModel.VocaListViewModel
 import com.example.devvoca.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
@@ -23,11 +28,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_favorite, R.id.navigation_my_page
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navView.setupWithNavController(navController)
+/*
         binding.inputButton1.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 VocaListViewModel.insertVoca(DataModel.localDB, VocaList(2,"예제를 넣어보자","Spelling","해석","예제","개발타입"))
@@ -42,6 +57,6 @@ class MainActivity : AppCompatActivity() {
                     Log.e("test","번호 : ${it.v_no} voca: ${it.voca}")
                 }
             }
-        }
+        }*/
     }
 }
