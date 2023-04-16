eef92350d9e3b6e4ea1908845a8cd72f8d6aaca1
Flasas <flasas@naver.com>
2023-04-16 16:01:08 +0900

    HomeFragment UI 수정 진행

--- app/src/main/java/com/example/devvoca/HomeFragment.kt
+++ app/src/main/java/com/example/devvoca/HomeFragment.kt
@@ -30,27 +30,6 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        (activity as AppCompatActivity?)!!.setSupportActionBar(binding.appBarMain.toolbar)
-
-
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
         return binding.root
     }
 
